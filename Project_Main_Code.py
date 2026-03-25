89c7353bb648f3ae7ffab7e16d05b4b14072de90
EdiB21 <eb614@kent.ac.uk>
2026-03-25 16:48:24 +0000

    user login and register added to code and completed with some comments, starting the menu usability portion

--- Project_Main_Code.py
+++ Project_Main_Code.py
@@ -22,7 +22,7 @@ def main():
 def login_screen():
     print("Welcome To The Awesome App!")
     time.sleep(0.3)
-    print("Please select an option:")
+    print("Please select an option:")   #simple opening screen with two options
     print("--------------------")
     print("Login ----- Register")
     print("--------------------")
@@ -42,11 +42,82 @@ def login_screen():
 
 
 def user_login():
+    print("Welcome Back, Please enter your Email and Password!")
+
+    attempts = 0
+    max_attempts = 3
+
+    while attempts < max_attempts:
+
+        email = REGEX_Check.email_format()      #only checks email and password like most other apps
+        time.sleep(0.3)
+        password = REGEX_Check.password_format()
+        time.sleep(0.3)
+        print("Checking Data Base..")
+        time.sleep(0.2)
+        print("...")
+        time.sleep(0.6)
+
+        with open("User_Data.csv", "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                if not row:
+                    continue
+                if row[1] == email and row[2] == password:
+                    print(f"Welcome back, {row[0].title()}!")  # uses the csv rows to check if the account has the correct role and then gives the correct menu options from the correct class for it
+                    if row[3] == "admin":
+                        user = User_Levels.Admin(row[0], row[1], row[2])
+                    elif row[3] == "reader":
+                        user = User_Levels.Reader(row[0], row[1], row[2])
+                    elif row[3] == "user":
+                        user = User_Levels.User(row[0], row[1], row[2])
+                    user_menu(user)
+                    return
+
+        attempts += 1
+        print("Credentials Do Not Match..")
+        print(f"{max_attempts - attempts} Attempts Remaining.")
+        time.sleep(0.3)
+
+    print("Too many failed attempts. Returning to Login...")
+    login_screen()
 
 
 def user_register():
+    print("Welcome! Please enter your name, email and a password!")
+
+    role = "user"
+
+    name = REGEX_Check.name_format()        #all inputs here and in the login function use the regex formatting to make sure that inputs are valid
+    time.sleep(0.4)                         # also used since they already have while loops to allow for multiple attempts and not accept invalid ones
+    print("Saving..")
+    time.sleep(0.4)
+    email = REGEX_Check.email_format()
+    time.sleep(0.4)
+    print("Saving..")
+    time.sleep(0.4)
+    password = REGEX_Check.password_format()
+    time.sleep(0.4)
+    print("Saving")
+    time.sleep(0.4)
+
+    with open("User_Data.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([name,email,password,role])
+    
+    print("Registration Complete!")
+    user = User_Levels.User(name, email, password)  #user variable saves the different parts after writing to make it easier to open the correct menu
+    user_menu(user)
+    time.sleep(0.2)
+    print("sending to menu..")
+    time.sleep(0.2)
 
 
+def user_menu(user):
+
+
+    
 if __name__ == "__main__":
     main()
 
