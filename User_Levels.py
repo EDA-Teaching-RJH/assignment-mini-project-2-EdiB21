cd3a1c46833b6301a44d49d45dc589b87aba8476
EdiB21 <eb614@kent.ac.uk>
2026-03-25 14:46:37 +0000

    admin class completed

--- User_Levels.py
+++ User_Levels.py
@@ -93,7 +93,7 @@ class Admin(User):
 
         with open(users_f, "a", newline="") as file:
             user_a = csv.writer(file)
-            user_a.writerow([user_na, user_em, user_pa, user_ro])
+            user_a.writerow([user_na, user_em, user_pa, user_ro])       #write to csv file on a new row (with statement uses 'a' to append)
 
         print("User has been added!")
         time.sleep(0.2)
@@ -101,6 +101,206 @@ class Admin(User):
         time.sleep(0.3)
         print("...")
         time.sleep(0.3)
+
+        with open(users_f, newline="") as file:
+            file_read = csv.DictReader(file)
+
+            print(f"{'Name':<15} {'Email':<45} {'Password':<30} {'Role':<10}")
+            print("-" * 100 + "|")
+
+            for row in file_read:
+                print(f"{(row['name'].title()):<15} {row['email']:<45} {row['password']:<30} {(row['role'].title()):<10}")      #make an organised table
+
+            print("-" * 100 + "|")
+
+
+    def remove_user(self):
+
+        users_f = "User_Data.csv"
+        admin_code = 2426
+        print("Removing User")
+        print("-" * 60)
+
+        user_em = input("Please enter the email of the user to remove: ").strip().lower()
+
+        with open(users_f, newline="") as file:
+            file_read = csv.DictReader(file)
+            a_rows = list(file_read)                    #this one saves the current rows in csv into a variable
+
+        all_emails = [row["email"] for row in a_rows]   #saves all emails in each row of the csv
+
+        print("FINDING USER...")
+        for i in range(1,5):
+            time.sleep(0.5)
+            print("." * i)
+
+        if user_em not in all_emails:
+            print("USER NOT FOUND")
+            time.sleep(0.2)
+            return
+
+        print("USER FOUND")
+
+        tries = 0
+        removed = False
+
+        while tries < 2:
+
+            admin_co = int(input("Please enter admin code: "))
+
+            if admin_co != admin_code:
+                tries += 1
+                if tries == 2:
+                    print("NO ATTEMPTS REMAINING")
+                    print("RETURNING TO MENU")
+                    return
+                print("INCORRECT ADMIN CODE")
+                print("ONE ATTTEMPT REMAINING")
+            else:
+                print("User is being removed")
+
+                for i in range(1,3):
+                    time.sleep(0.5)
+                    print("." * i)
+                 
+                new_rows = [row for row in a_rows if row["email"] != user_em]   #this is going through the rows and saving only the rows which dont
+                                                                                 #have the same email as our inputted one and then saving that as new rows 
+                with open(users_f, "w", newline="") as file:
+                    user_a = csv.DictWriter(file, fieldnames = ["name", "email", "password", "role"])
+                    user_a.writeheader()
+                    user_a.writerows(new_rows)              #re writes the entire file with our current new set of rows that doesnt include the one
+                print("User has been removed")              # we want to remove 
+                removed = True
+                break
+        if removed:            #if removed == True
+            print("New user database: ")
+            time.sleep(0.3)
+            print("...")
+            time.sleep(0.3)
+
+            with open(users_f, newline="") as file:
+                file_read = csv.DictReader(file)
+
+                print(f"{'Name':<15} {'Email':<45} {'Password':<30} {'Role':<10}")
+                print("-" * 100 + "|")
+
+                for row in file_read:
+                    print(f"{(row['name'].title()):<15} {row['email']:<45} {row['password']:<30} {(row['role'].title()):<10}")
+                print("-" * 100 + "|")
+
+
+    def upd_user(self):
+
+        users_f = "User_Data.csv"
+        admin_code = 2426
+        print("Updating User")
+        print("-" * 60)
+        time.sleep(0.5)
+
+        user_em = input("Please enter the email of the user to update: ").strip().lower()
+
+        with open(users_f, newline="") as file:
+            file_read = csv.DictReader(file)
+            a_rows = list(file_read)        #same premise as before saving all csv rows
+
+        all_emails = [row["email"] for row in a_rows]   #holds all the rows emails
+
+        print("FINDING USER...")
+        for i in range(1,5):
+            time.sleep(0.5)
+            print("." * i)
+
+        if user_em not in all_emails:       #cross checks our input is in the emails
+            print("USER NOT FOUND")
+            time.sleep(0.2)
+            return
+
+        print("USER FOUND")
+
+        tries = 0
+        updated = False
+
+        while tries < 2:
+
+            admin_co = int(input("Please enter admin code: "))
+
+            if admin_co != admin_code:
+                tries += 1
+                if tries == 2:
+                    print("NO ATTEMPTS REMAINING")
+                    print("RETURNING TO MENU")
+                    return
+                print("INCORRECT ADMIN CODE")
+                print("ONE ATTTEMPT REMAINING")
+            else:
+                updating = True
+
+                while updating:
+                    upd_choice = input("Would you like to update the: [Name] [Email] [Password] [Role] [Done] \n:").strip().lower()
+                
+                    if upd_choice == "name":
+                        new_val = REGEX_Check.name_format()
+                        for row in a_rows:
+                            if row["email"] == user_em:
+                                row["name"] = new_val
+                        print("Name updated!")
+                    elif upd_choice == "email":
+                        new_val = REGEX_Check.email_format()
+                        for row in a_rows:
+                            if row["email"] == user_em:
+                                row["email"] = new_val
+                        user_em = new_val
+                        print("Email updated!")
+                    elif upd_choice == "password":
+                        new_val = REGEX_Check.password_format()
+                        for row in a_rows:
+                            if row["email"] == user_em:
+                                row["password"] = new_val
+                        print("Password updated!")
+                    elif upd_choice == "role":
+                        new_val = input("Enter the new role: ").strip().lower()
+                        for row in a_rows:
+                            if row["email"] == user_em:
+                                row["role"] = new_val
+                        print("Role updated!")
+                    elif upd_choice == "done":
+                        updating = False
+                    else:
+                        print("Invalid entry, try again")
+
+                with open(users_f, "w", newline="") as file:        #once all changes are made and done is selected loop ends and the new a_rows is writen to the csv file
+                    user_a = csv.DictWriter(file, fieldnames=["name","email","password","role"])
+                    user_a.writeheader()
+                    user_a.writerows(a_rows)
+
+                print("Changes Saved")
+                updated = True
+                break
+
+        if updated:
+            print("New user database: ")
+            time.sleep(0.3)
+            print("...")
+            time.sleep(0.3)
+
+            with open(users_f, newline="") as file:
+                file_read = csv.DictReader(file)
+
+                print(f"{'Name':<15} {'Email':<45} {'Password':<30} {'Role':<10}")
+                print("-" * 100 + "|")
+
+                for row in file_read:
+                    print(f"{(row['name'].title()):<15} {row['email']:<45} {row['password']:<30} {(row['role'].title()):<10}")
+                print("-" * 100 + "|")
+
+
+    def view_data(self):
+        users_f = "User_Data.csv" 
+
+        print("Printing Current Database: ")
+        for i in range(1,3):
+            time.sleep(0.5)
+            print("." * i)
 
         with open(users_f, newline="") as file:
             file_read = csv.DictReader(file)
