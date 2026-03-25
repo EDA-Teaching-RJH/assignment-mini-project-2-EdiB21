dc1f98a53063dc488acd5a61d9685275b5e1fc3c
EdiB21 <eb614@kent.ac.uk>
2026-03-25 14:58:01 +0000

    starting the testing file to make sure all is working before connecting all codes/files

---
+++ R_STR_Test_File.py
@@ -0,0 +1,14 @@
+import REGEX_Check
+import User_Levels
+import csv
+import re
+import unittest  #tried another testing library apart from pytest as it wasnt working for me
+
+
+class TestRegexFormatting(unittest.TestCase):
+
+
+class TestUserLevels(unittest.TestCase):
+
+
+class TestMenuOptions(unittest.TestCase):
