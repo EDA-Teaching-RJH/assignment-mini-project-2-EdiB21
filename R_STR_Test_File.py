49802a0fd93e5cdfbf810947fcd9bebe78a47093
EdiB21 <eb614@kent.ac.uk>
2026-03-25 16:32:52 +0000

    test file is complete and comments included

--- R_STR_Test_File.py
+++ R_STR_Test_File.py
@@ -34,7 +34,48 @@ class TestRegexFormatting(unittest.TestCase):
         self.assertFalse(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "wr.ong@email.uk.brz"))
 
 
-class TestUserLevels(unittest.TestCase):
+class TestUserLevels(unittest.TestCase):        #overall checking that classes system works and roles are assigned correctly
+
+    def test_admin_role(self):
+        admin = User_Levels.Admin("edis", "edis@yahoo.com", "apple123")
+        self.assertEqual(admin.role, "admin")
+
+
+    def test_user_cant_manage(self):
+        user = User_Levels.User("bob", "bobo@gmail.com", "banana5")
+        self.assertFalse(user.has_permission("manage_users"))
+
+
+    def test_user_can_view_self(self):
+        user = User_Levels.User("bob", "bobo@gmail.com", "banana5")
+        self.assertTrue(user.has_permission("view_self"))
+
+
+    def test_reader_subclass_user(self):
+        self.assertIsSubclass(User_Levels.Reader, User_Levels.User)     #checking if the classes are correctly subbed from User
+
+
+    def test_reader_not_subclass_admin(self):
+        self.assertFalse(issubclass(User_Levels.Admin, User_Levels.Reader))
 
 
 class TestMenuOptions(unittest.TestCase):
+
+    def test_reader_menu(self):
+        reader = User_Levels.Reader("edi", "edicool@bingo.co.uk", "pass258")
+        self.assertIn("4. View User Information", reader.menu_options())        #checks that options are in menu options
+
+
+    def test_reader_cant_remove(self):
+        reader = User_Levels.Reader("edi", "edicool@bingo.co.uk", "pass258")        #checks that reader doesnt have admin options
+        self.assertNotIn("6. Remove User", reader.menu_options())
+
+
+    def test_admin_mangement(self):
+        admin = User_Levels.Admin("edis", "myemail@gmail.com", "monkey29")
+        self.assertIn("7. Update User Role", admin.menu_options())
+
+
+    def test_admin_no_extras(self):
+        admin = User_Levels.Admin("edis", "myemail@gmail.com", "monkey29")      #checks that the option is not in menu options
+        self.assertNotIn("9. Be silly", admin.menu_options())
