2fdda7a8f723ecc48cce97b019e6aaf4e18c9e3f
EdiB21 <eb614@kent.ac.uk>
2026-03-25 15:08:05 +0000

    regex testing done

--- R_STR_Test_File.py
+++ R_STR_Test_File.py
@@ -6,6 +6,32 @@ import unittest  #tried another testing library apart from pytest as it wasnt wo
 
 
 class TestRegexFormatting(unittest.TestCase):
+    
+    def test_name(self):
+        self.assertTrue(re.match(r'^[a-zA-Z]+$', "Timothy"))
+        self.assertTrue(re.match(r'^[a-zA-Z]+$', "Bradley"))
+    
+
+    def test_name_digits(self):
+        self.assertFalse(re.match(r'^[a-zA-Z]+$', "B0bby5"))
+        self.assertFalse(re.match(r'^[a-zA-Z]+$', "Wr0ng5"))
+
+    
+    def test_name_specials(self):
+        self.assertFalse(re.match(r'^[a-zA-Z]+$', "B0bby/5"))
+        self.assertFalse(re.match(r'^[a-zA-Z]+$', "b4d/name"))
+
+
+    def test_email(self):
+        self.assertTrue(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "edis@kent.ac.uk"))
+        self.assertTrue(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "awesomegamer23@gmail.com"))
+        self.assertTrue(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "correct@email.co.uk"))
+    
+
+    def test_email_inva(self):
+        self.assertFalse(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "bob#'@bobbyiscool.co.cz"))
+        self.assertFalse(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "in/correct@email.co.uk"))
+        self.assertFalse(re.match(r'^\w+@\w+\.(ac\.uk|gov\.uk|com|co\.uk)$', "wr.ong@email.uk.brz"))
 
 
 class TestUserLevels(unittest.TestCase):
